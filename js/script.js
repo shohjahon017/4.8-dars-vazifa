9a6964c992ca49886f2e966e404af42bc2985bf5
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-31 01:42:34 +0500

    ...

--- js/script.js
+++ js/script.js
@@ -40,3 +40,65 @@
 //   return p;
 // }
 // console.log(multiplyElement(arr));
+
+//5-masala
+// let arr = [1, 5, 3, "a", 5, "f"];
+// function sortElement(arg) {
+//   let res = arg.sort();
+//   return res;
+// }
+// console.log(sortElement(arr));
+
+//6-masala
+// let arr = ["salom", 5, 6, 9, 8];
+// function reverseElement(arg) {
+//   let res = arg.reverse();
+//   return res;
+// }
+// console.log(reverseElement(arr));
+
+//7-masala
+// let arr = ["salom", "xayr", "hello", 6, 8];
+// function indexElement(arg) {
+//   let res = [];
+//   for (let i = 0; i < arg.length; i++) {
+//     res.push(i);
+//   }
+//   return res;
+// }
+// console.log(indexElement(arr));
+
+//8-masala
+// let arr = [4, 69, 3, 45, 6, 9];
+// function indexElement(arg) {
+//   let res = arg.indexOf(3);
+//   return res;
+// }
+// console.log(indexElement(arr));
+
+//9-masala
+// let arr = [1, 6, 8, 9, 2, 5];
+// function addElement(arg, num) {
+//   let res = [];
+//   arg.forEach((element) => {
+//     res.push(element + num);
+//   });
+//   return res;
+// }
+// console.log(addElement(arr, 5));
+
+//10-masala
+// let arr = [4, 5, 3, 6, 1, 2, 7, 9, 8];
+// function oddEvenElement(arg) {
+//   let odd = [];
+//   let even = [];
+//   for (const iterator of arg) {
+//     if (iterator % 2 == 1) {
+//       odd.push(iterator);
+//     } else {
+//       even.push(iterator);
+//     }
+//     return even;
+//   }
+// }
+// console.log(oddEvenElement(arr));
