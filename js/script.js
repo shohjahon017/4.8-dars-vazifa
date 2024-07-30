e703ee8fc133a0be2a76dc61de5b86dfeb886649
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-30 22:55:24 +0500

    ..

--- js/script.js
+++ js/script.js
@@ -7,14 +7,36 @@
 // }
 
 //2-masala
-let arr = [5, "salom", 9, 6, 3, "dunyo"];
-function concatString(arg) {
-  let res = [];
-  arg.forEach((element) => {
-    if (typeof element == "string") {
-      res.push(element);
-    }
-  });
-  return res;
-}
-console.log(concatString(arr));
+// let arr = [5, "salom", 9, 6, 3, "dunyo"];
+// function concatString(arg) {
+//   let res = [];
+//   arg.forEach((element) => {
+//     if (typeof element == "string") {
+//       res.push(element);
+//     }
+//   });
+//   return res;
+// }
+// console.log(concatString(arr));
+
+//3-masala
+// let arr = ["salom", "xayr", "hello", "bye"];
+// function toUpperCaseElement(arg) {
+//   let res = [];
+//   arg.forEach((element) => {
+//     res.push(element.toUpperCase());
+//   });
+//   return res;
+// }
+// console.log(toUpperCaseElement(arr));
+
+//4-masala
+// let arr = [4, 6, 9, 6, 3, 1];
+// function multiplyElement(arg) {
+//   let p = 1;
+//   arg.forEach((element) => {
+//     p *= element;
+//   });
+//   return p;
+// }
+// console.log(multiplyElement(arr));
